bc0116861818ad9306d1a34b15e22013c328c018
Vasiliy Brave <vskh93.009@gmail.com>
2021-12-26 16:08:57 +0300

    - вторая версия приложения.

---
+++ app/src/main/java/com/hfad/tictactoemvpappversiontwo/MainActivity.java
@@ -0,0 +1,166 @@
+package com.hfad.tictactoemvpappversiontwo;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView playerOneScore, playerTwoScore, playerStatus;
+    private final Button[] buttons = new Button[9];
+    private Button resetGame;
+
+    private int playerOneScoreCount, playerTwoScoreCount, roundCount;
+    boolean activePlayer;
+
+    // p1 => 0;
+    // p2 => 1;
+    // empty => 2;
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+
+    int[][] winningPosition = {
+            {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, //rows
+            {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, //columns
+            {0, 4, 8}, {2, 4, 6} // cross
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        playerOneScore = (TextView) findViewById(R.id.playerOneScore);
+        playerTwoScore = (TextView) findViewById(R.id.playerTwoScore);
+        playerStatus = (TextView) findViewById(R.id.playerStatus);
+
+        resetGame = (Button) findViewById(R.id.resetGame);
+
+        for (int i = 0; i < buttons.length; i++) {
+            String buttonID = "btn_" + i;
+            int resourceID = getResources().getIdentifier(buttonID, "id", getPackageName());
+            buttons[i] = (Button) findViewById(resourceID);
+            buttons[i].setOnClickListener(this);
+        }
+
+        roundCount = 0;
+        playerOneScoreCount = 0;
+        playerTwoScoreCount = 0;
+        activePlayer = true;
+    }
+
+    @Override
+    public void onClick(View view) {
+        // Log.i("test", "button is clicked!");
+        if (!((Button) view).getText().toString().equals("")) {
+            return;
+        }
+        String buttonID = view.getResources().getResourceEntryName(view.getId());
+        int gameStatePointer = Integer.parseInt(buttonID.substring(buttonID.length() - 1, buttonID.length()));
+
+        if (activePlayer) {
+            ((Button) view).setText("X");
+            ((Button) view).setTextColor(Color.parseColor("#FFC34A"));
+            gameState[gameStatePointer] = 0;
+        } else {
+            ((Button) view).setText("0");
+            ((Button) view).setTextColor(Color.parseColor("#70FFEA"));
+            gameState[gameStatePointer] = 1;
+
+        }
+        roundCount++;
+
+        if (checkWinner()) {
+            if (activePlayer) {
+                playerOneScoreCount++;
+                updatePlayerScore();
+                Toast.makeText(this, "Player One Won!", Toast.LENGTH_SHORT).show();
+                playAgain();
+            } else {
+                playerTwoScoreCount++;
+                updatePlayerScore();
+                Toast.makeText(this, "Player Two Won!", Toast.LENGTH_SHORT).show();
+                playAgain();
+            }
+        } else if (roundCount == 9) {
+            playAgain();
+            Toast.makeText(this, "No Winner!", Toast.LENGTH_SHORT).show();
+        } else {
+            activePlayer = !activePlayer;
+        }
+        if (playerOneScoreCount > playerTwoScoreCount) {
+            playerStatus.setText("Player One is Winning!");
+        } else if (playerTwoScoreCount > playerOneScoreCount) {
+            playerStatus.setText("Player Two is Winning!");
+
+        } else {
+            playerStatus.setText("");
+        }
+        resetGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playAgain();
+                playerOneScoreCount = 0;
+                playerTwoScoreCount = 0;
+                playerStatus.setText("");
+                updatePlayerScore();
+            }
+        });
+    }
+
+    public boolean checkWinner() {
+        boolean winnerResult = false;
+
+        for (int[] winningPosition : winningPosition) {
+            if (gameState[winningPosition[0]] == gameState[winningPosition[1]] &&
+                    gameState[winningPosition[1]] == gameState[winningPosition[2]] &&
+                    gameState[winningPosition[0]] != 2) {
+                winnerResult = true;
+
+            }
+        }
+        return winnerResult;
+    }
+
+    public void updatePlayerScore() {
+        playerOneScore.setText(Integer.toString(playerOneScoreCount));
+        playerTwoScore.setText(Integer.toString(playerTwoScoreCount));
+    }
+
+    public void playAgain() {
+        roundCount = 0;
+        activePlayer = true;
+        for (int i = 0; i < buttons.length; i++) {
+            gameState[i] = 2;
+            buttons[i].setText("");
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@Nullable Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+
+        savedInstanceState.putInt("roundCount", roundCount);
+        savedInstanceState.putInt("playerOneScoreCount", playerOneScoreCount);
+        savedInstanceState.putInt("playerTwoScoreCount", playerTwoScoreCount);
+        savedInstanceState.putBoolean("activePlayer", activePlayer);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        roundCount = savedInstanceState.getInt("roundCount");
+        playerOneScoreCount = savedInstanceState.getInt("playerOneScoreCount");
+        playerTwoScoreCount = savedInstanceState.getInt("playerTwoScoreCount");
+        activePlayer = savedInstanceState.getBoolean("activePlayer");
+
+    }
+}
